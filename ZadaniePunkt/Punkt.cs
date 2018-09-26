2b541f815082876409489c83eb49cf2a6b376a9a
Tomasz Dawid <tomasz.dawid@balluff.pl>
2018-09-26 11:19:25 +0200

    dodane przeciazenia operatorow

--- ZadaniePunkt/Punkt.cs
+++ ZadaniePunkt/Punkt.cs
@@ -55,5 +55,41 @@ namespace ZadaniePunkt
             Console.WriteLine("Współrzędna Y: {0}",Y);
         }
 
+        public static Punkt operator +(Punkt p1, Punkt p2) // nie jest to dodawanie fizyczne - jest to tylko definicja operatora "+" dla dodawania klas.
+        {
+            int x = p1.X + p2.X;
+            int y = p1.Y + p2.Y;
+
+            Punkt result = new Punkt(x, y);
+            //Punkt result = new Punkt(p1.X + p2.X, p1.Y + p2.Y); - inny sposób obliczenia
+            return result;
+        }
+
+        public static Punkt operator -(Punkt p1, Punkt p2) // nie jest to dodawanie fizyczne - jest to tylko definicja operatora "+" dla dodawania klas.
+        {
+            int x = p1.X - p2.X;
+            int y = p1.Y - p2.Y;
+
+            Punkt result = new Punkt(x, y);
+            
+            return result;
+        }
+
+        public static Punkt operator *(Punkt p1, int p2)
+        {
+            int x = p1.X * p2;
+            int y = p1.Y * p2;
+
+            Punkt result = new Punkt(x,y);
+
+            return result;
+        }
+
+        public static Punkt operator *(int m, Punkt p1)
+        {
+            
+            return p1 * m; // operator przeciążony "*" jest wykorzystywany z deklaracji powyżej czyli wykonuje mnożenie zdefiniowane w "public static Punkt operator *(Punkt p1, int p2)"
+        }
+
     }
 }
